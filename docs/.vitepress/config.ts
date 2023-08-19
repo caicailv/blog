70e26823c1568da12687af7bcd2e48134b32b6b5
菜菜驴 <46363316+caicailv@users.noreply.github.com>
2023-08-20 00:45:18 +0800

    first commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,10 @@
+// import { defineConfig } from 'vitepress'
+// export default defineConfig({
+//   lang: 'en-US',
+//   title: 'ccl',
+//   description: 'Vite & Vue powered static site generator.',
+// })
+export default {
+  title: '菜菜驴',
+  description: 'Vite & Vue powered static site generator.',
+}
