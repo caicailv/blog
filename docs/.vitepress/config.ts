099078547a8dee17458cc7456d653f652c948c21
菜菜驴 <46363316+caicailv@users.noreply.github.com>
2023-11-01 00:47:36 +0800

    delete img

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,11 +1,11 @@
-// import { defineConfig } from 'vitepress'
-// export default defineConfig({
-//   lang: 'en-US',
-//   title: 'ccl',
-//   description: 'Vite & Vue powered static site generator.',
-// })
+import imgLazy from 'markdown-it-img-lazy'
+import imgStyle from './imgStyle'
+console.log('abc');
 export default {
   title: '菜菜驴',
   ignoreDeadLinks: true,
   description: 'Vite & Vue powered static site generator.',
+  config(md) {
+    md.use(imgStyle).use(imgLazy)
+  },
 }
